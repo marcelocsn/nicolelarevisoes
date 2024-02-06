a8151a619802f59e955f1c3c96336d388a17a82b
Marcelo  Castro <csnicolela.marcelo@gmail.com>
2024-02-06 19:44:13 -0300

    Add files via upload

---
+++ javascript/menu.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let btnMenu = document.getElementById('btn-menu');
+    let menuMobile = document.getElementById('menu-mobile');
+    let btnFecharMenu = document.getElementById('btn-fechar-menu');
+    let overlayMenu = document.getElementById('overlay-menu');
+    let linksMenuMobile = document.querySelectorAll('.menu-mobile-content ul li a'); // Ajuste o seletor para a nova classe
+
+    btnMenu.addEventListener('click', () => {
+        console.log('Botão Menu clicado');
+        menuMobile.classList.add('abrir-menu');
+        overlayMenu.style.display = 'block';
+    });
+
+    btnFecharMenu.addEventListener('click', () => {
+        console.log('Botão Fechar Menu clicado');
+        menuMobile.classList.remove('abrir-menu');
+        overlayMenu.style.display = 'none';
+    });
+
+    overlayMenu.addEventListener('click', () => {
+        console.log('Overlay clicado');
+        menuMobile.classList.remove('abrir-menu');
+        overlayMenu.style.display = 'none';
+    });
+
+    // Adicione um ouvinte de evento para cada link no menu móvel
+    linksMenuMobile.forEach(link => {
+        link.addEventListener('click', () => {
+            console.log('Link do Menu Móvel clicado');
+            menuMobile.classList.remove('abrir-menu');
+            overlayMenu.style.display = 'none';
+        });
+    });
+});
